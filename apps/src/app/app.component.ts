bc9adb2f2e30a6046bf28bc53bc0620b1d9cbb27
Marina <marinaysn@gmail.com>
2019-05-06 21:34:43 -0400

    adding more styles including test to inline styles

--- apps/src/app/app.component.ts
+++ apps/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css', './newStyle.css']
+
+  styles: [`
+    h3{
+      color: green;
+    }
+  `]
 })
 export class AppComponent {
   // name = 'Marina\'s App';
