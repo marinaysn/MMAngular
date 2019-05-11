16bb6df37ff455c9ece3a29d42357d5502a436f5
Marina <marinaysn@gmail.com>
2019-05-11 13:30:29 -0400

    add styles

--- apps/src/app/server/server.component.ts
+++ apps/src/app/server/server.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
+import { getRenderedText } from '@angular/core/src/render3';
+import { stringify } from '@angular/core/src/util';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [
+    `.online {
+        color: white;
+    }`
+]
 })
 
 export class ServerComponent {
     serverId = 10;
-    serverStatus = 'offline'
+    serverStatus: string = 'offline'
+
+    constructor () {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
