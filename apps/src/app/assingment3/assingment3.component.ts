3ee8b9cb5da54746d25523eaae1da3bdc8753aca
Marina <marinaysn@gmail.com>
2019-05-11 22:03:56 -0400

    assingment 3

--- apps/src/app/assingment3/assingment3.component.ts
+++ apps/src/app/assingment3/assingment3.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Assingment3Component implements OnInit {
 
+  switchDisplay: boolean = false;
+  buttonName: string = 'Display Details';
+  secretPassword: string = "Secret Password = tuna";
+  logIt: string[] = [''];
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  onClickHideDisplay() {
+  
+    if (this.switchDisplay) {
+      this.buttonName = 'Display Details'  
+      this.logIt.push(this.logIt.length + '. ' + new Date().toLocaleString() );
+    }
+    else {
+      this.buttonName = 'Hide Details'
+    }
+    this.switchDisplay = !this.switchDisplay 
+  }
 }
