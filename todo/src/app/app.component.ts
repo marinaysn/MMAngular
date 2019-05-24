53668abe0f6b6fc593507e059e9f5b84c1da2299
Marina <marinaysn@gmail.com>
2019-05-23 21:51:20 -0400

    adding procedures for TODO app

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Model } from "./model";
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'todo-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo';
+  model = new Model();
+
+  getName() {
+    return this.model.user;
+  }
+
 }
