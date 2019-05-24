9115455ba4d0b7f8d2d2bcc4ca918dfe761bf1b9
Marina <marinaysn@gmail.com>
2019-05-24 16:59:31 -0400

    completed ch2. wrap up

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Model } from "./model";
+import { Model, TodoItem } from "./model";
 import { isNgTemplate } from '@angular/compiler';
+import { modelGroupProvider } from '@angular/forms/src/directives/ng_model_group';
 
 
 @Component({
@@ -12,12 +13,18 @@ export class AppComponent {
   title = 'todo';
   model = new Model();
 
+
   getName() {
     return this.model.user;
   }
 
   getTodoItems(){
-    return this.model.items.filter((i: { done: boolean; }) =>!i.done);
+    // return this.model.items.filter((i: { done: boolean; }) =>!i.done);
+    return this.model.items;
   }
-
+  addTodoItem(item) {
+    if (item !==""){
+      this.model.items.push(new TodoItem(item, false));
+    }
+  }
 }
