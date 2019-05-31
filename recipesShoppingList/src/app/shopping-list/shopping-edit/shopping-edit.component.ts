fd00fce32dafb19591aac074b341cf850b7b66a9
Marina <marinaysn@gmail.com>
2019-05-30 22:20:04 -0400

    added ability for user to add items to shopping list

--- recipesShoppingList/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipesShoppingList/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+ 
+  printNameRef: string = "";
+  printAmountRef: string = "";
+
+  ingridience = [];
+
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
   }
+ 
+  onNameInput(){
+    console.log('1111' + this.nameInputRef.nativeElement.value);
 
+    this.printNameRef = this.nameInputRef.nativeElement.value;
+    this.printAmountRef = " - " + this.amountInputRef.nativeElement.value;
+
+    this.ingridience.push({name: this.printNameRef, num: this.printAmountRef})
+  }
+
+  onDelete() {
+    this.ingridience = [];
+  }
 }
