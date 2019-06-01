38053a082d8de72e0b6ab6210658958da9f69c5e
Marina <marinaysn@gmail.com>
2019-06-01 13:02:29 -0400

    recap of ng-If and ng-For

--- directives-start/src/app/app.component.ts
+++ directives-start/src/app/app.component.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  numbers = [1, 2, 3, 4, 5];
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1,3,5,7,9,11];
+  evenNumbers =[2,4,6,8,10,12];
   onlyOdd = false;
+  title: string = "Show ODD numbers";
+  title2: string = "EVEN numbers:";
+
+  onClick(){
+    this.onlyOdd = !this.onlyOdd;
+
+    if (this.onlyOdd) {
+    this.title = "Show EVEN numbers";
+    this.title2 = "ODD numbers:";
+    }
+
+    else {
+      this.title = "Show ODD numbers";
+      this.title2 = "EVEN numbers:";
+    }
+  }
 }
