59a0b7c0651e941b6ff6f07e62ac4dc1af3939a7
Marina <marinaysn@gmail.com>
2019-06-13 00:19:00 -0400

    adding more functionality to shopping list

--- recipesShoppingList/src/app/recipes/RecipeService.service.ts
+++ recipesShoppingList/src/app/recipes/RecipeService.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
 
 
 export class RecipeService {
@@ -8,13 +9,23 @@ export class RecipeService {
 
     private recipes: Recipe[] = [
         new Recipe('Bacon-wrapped Burger Roll Recipe', 'Hot Italian Rolls', 
-        'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg')
+        'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg',
+        [new Ingredient("tomatoes", 2),
+        new Ingredient("bread", 1)])
         ,
         new Recipe('Supreme Pizza Recipe', 'Supreme Pizza a la Italian in New York', 
-        'https://upload.wikimedia.org/wikipedia/commons/d/d3/Supreme_pizza.jpg')
+        'https://upload.wikimedia.org/wikipedia/commons/d/d3/Supreme_pizza.jpg',
+        [new Ingredient("potatos", 2),
+        new Ingredient("milk", 1)])
         ,
+
         new Recipe('Chicken Rolls Recipe', 'Chicken Rolls', 
-        'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg')
+        'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg',
+        [new Ingredient("meat", 5),
+        new Ingredient("buns", 5),
+        new Ingredient("mayo", 2),
+        new Ingredient("lettuce", 1)
+        ])
       ];
 
       getRecipes() {
--- recipesShoppingList/src/app/recipes/recipe.model.ts
+++ recipesShoppingList/src/app/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from '../shared/Ingredient.model';
+
 export class Recipe {
 
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name: string, desc: string, img: string) {
+    constructor(name: string, desc: string, img: string, ingredience: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = img;
+        this.ingredients= ingredience;
     }
 }
