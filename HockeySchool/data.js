1115e8425c21a516a4393002bafaebe5f1429ed5
Marina <marinaysn@gmail.com>
2019-06-20 07:45:24 -0400

    added hockey school private project

---
+++ HockeySchool/data.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+    return {
+    products: [
+    { id: 1, name: "1-on-1", category: "Monday Class",
+    description: "This is Monday Class 1-on-1 with coach Rick", price: 50 },
+    { id: 2, name: "1-on-3", category: "Monday Class",
+    description: "This is Monday Class 1-on-2 with coach Rick", price: 60 },
+    { id: 3, name: "1-on-1", category: "Tuesday Class",
+    description: "This is Tuesday Class 1-on-1 with coach Rick", price: 50 },
+    { id: 4, name: "1-on-3", category: "Tuesday Class",
+    description: "This is Tuesday Class 1-on-2 with coach Rick", price: 60 }
+],
+orders: []
+}
+}
