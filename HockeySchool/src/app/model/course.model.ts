450a41fecd47a74291c618980389073f44072336
Marina <marinaysn@gmail.com>
2019-06-21 19:22:20 -0400

    adding model and static data sourse

---
+++ HockeySchool/src/app/model/course.model.ts
@@ -0,0 +1,10 @@
+export class Course {
+    constructor(
+        public id: number,
+        public name: string,
+        public category?: string,
+        public description?: string,
+        public price?: number
+    )
+    { }
+}
