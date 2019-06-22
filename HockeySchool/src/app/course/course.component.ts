5b6669c39a4df26b94a52d900dbace3a3b9bd2df
Marina <marinaysn@gmail.com>
2019-06-21 23:47:08 -0400

    added categories sections

--- HockeySchool/src/app/course/course.component.ts
+++ HockeySchool/src/app/course/course.component.ts
@@ -2,15 +2,16 @@ import { Component } from "@angular/core";
 import { CourseRepository } from "../model/course.repository";
 import { Course } from "../model/course.model";
 
-
 @Component({
-
     selector: "course",
     templateUrl: "course.component.html",
     styleUrls: ["course.component.css"]
 })
 
 export class CourseComponent{
+
+    public selectedCategory = null;
+    
     constructor(private repo: CourseRepository){}
 
     get courses(): Course[] {
